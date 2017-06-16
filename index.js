e52c0d2da8f3b06425f20088c11acff2aa78f6ce
Tommy Byrd <tcbyrd@github.com>
2017-06-16 00:01:33 -0400

    Adding some comments and error handling

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
+// Webpack setup
 require.include('probot')
-const createProbot = require('./probot');
 const fs = require('fs')
-
 require('file-loader?name=private-key.pem!./private-key.pem')
 const cert = fs.readFileSync('private-key.pem', 'utf8')
 
+// Probot setup
+const createProbot = require('./probot');
 const probot = createProbot({
   id: process.env.APP_ID,
   secret: process.env.WEBHOOK_SECRET,
   cert: cert,
   port: 0
 });
-
+// Load the Probot plugin
 probot.load(require('./autoresponder'));
 
+// Lambda Handler
 module.exports.probotHandler = function (event, context, callback) {
+  // Determine incoming webhook event type
   const e = event.headers['X-GitHub-Event']
-  const payload = JSON.parse(event.body)
-  const id = event.headers['X-GitHub-Delivery']
 
-  probot.robot.webhook.emit(e, {
-    id: id,
-    payload: payload
-  })
-
-  const res = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Executed',
-      payload: payload
+  try { // Do the thing
+    probot.robot.webhook.emit(e, {
+      id: event.headers['X-GitHub-Delivery'],
+      payload: JSON.parse(event.body)
     })
-  }
 
-  callback(null, res)
+    const res = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Executed'
+      })
+    }
+    callback(null, res)
+  } catch (err) {
+    console.log(err)
+    callback(err)
+  }
 
 }
