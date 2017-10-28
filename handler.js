c771abd0e331a7e4e2f977b04164047a070598c5
tcbyrd <tcbyrd@github.com>
2017-10-28 18:31:29 -0400

    Move webhook receiver back to its own function.

--- handler.js
+++ handler.js
@@ -17,13 +17,37 @@ const probot = createProbot({
 
 // Load the local probot plugins
 probot.setup([require('./plugins/autoresponder')])
+module.exports.webhook = function (event, context, callback) {
+  // Determine incoming webhook event type
+  // Checking for different cases since node's http server is lowercasing everything
+  const e = event.headers['x-github-event'] || event.headers['X-GitHub-Event']
 
-// Proxy the lambda event handler into `probot.server```
-const serverless = require('serverless-http')
+  // Convert the payload to an Object if API Gateway stringifies it
+  event.body = (typeof event.body === 'string') ? JSON.parse(event.body) : event.body
+
+  // Do the thing
+  probot.receive({
+    event: e,
+    payload: event.body
+  })
+  .then(() => {
+    const res = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Executed'
+      })
+    }
+    callback(null, res)
+  })
+}
+
 const express = require('express')
 const path = require('path')
 // Override the default folders so they can be accessed
 probot.server.use('/probot/static', express.static(path.join(process.cwd(), 'static')))
 probot.server.set('views', path.join(process.cwd(), 'views'))
 
-module.exports.router = serverless(probot.server, { callbackWaitsForEmptyEventLoop: true })
+const awsServerlessExpress = require('aws-serverless-express')
+const probotServer = awsServerlessExpress.createServer(probot.server)
+
+exports.router = (event, context) => awsServerlessExpress.proxy(probotServer, event, context)
